e702797e90e73b9e4958a73ab8a1f6d06109dba8
Italo Silva Guanilo <italosilva@uni.pe>
2022-06-05 18:20:32 -0500

    Pregunta7 - Diseño  de la logica de la clase PremiumFlight

--- src/main/java/Fase5/Produccion/PremiumFlight.java
+++ src/main/java/Fase5/Produccion/PremiumFlight.java
@@ -3,4 +3,20 @@ package Fase5.Produccion;
 public class PremiumFlight extends Flight {
 
    // Diseño de la lógica comercial para los pasajeros VIP.Pregunta 7
+   public PremiumFlight(String id) {
+       super(id);
+   }
+
+    @Override
+    public boolean addPassenger(Passenger passenger) {
+        if(passenger.isVip()){
+            return passengers.add(passenger);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removePassenger(Passenger passenger) {
+        return passengers.remove(passenger);
+    }
 }
